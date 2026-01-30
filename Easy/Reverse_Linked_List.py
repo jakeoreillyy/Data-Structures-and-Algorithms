1e8391b0d4b7a73bb27d27ddc94bc7feed69e692
jakeoreilly <C24321986@mytudublin.ie>
2026-01-30 11:41:14 +0000

    Adding Reverse Linked List

---
+++ Easy/Reverse_Linked_List.py
@@ -0,0 +1,29 @@
+"""
+Reverse_Linked_List.py
+LeetCode problem: 206
+Difficulty: Easy
+Description: Reverses a singly linked list. i.e. [1,2,3,4,5] becomes [5,4,3,2,1]
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        before = None
+        after = head
+
+        while after:
+            after = head.next
+            head.next = before
+            before = head
+            head = after
+        return before
+        
