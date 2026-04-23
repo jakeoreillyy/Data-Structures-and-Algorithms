c2645fe069871d6a2c85f523766d4598f9aada27
Jake <oreillyjake16@gmail.com>
2026-04-23 11:58:52 +0100

    Revert "Cleaner solution"
    This reverts commit 309ace5118fd6eff2432c06c8e38a2e6552cb342.

--- Neetcode/Blind_75/contains_duplicate.py
+++ Neetcode/Blind_75/contains_duplicate.py
@@ -1,10 +1,14 @@
 """
 Contains Duplicate
 Difficulty: Easy
-Description: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct. i.e. Given nums = [1,2,3,1], the output should be true because the value 1 appears twice in the array.
+Description: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct. i.e. Given nums = [1,2,3,1], the output should be true because the value 1 appears twice in the array.   
 """
-
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+        group = set()
+        for num in nums:
+            if num in group:
+                return True
+            group.add(num)
+        return False
