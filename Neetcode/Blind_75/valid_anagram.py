c839d9795787569688efc8b5c4ba4e62513b8693
Jake <oreillyjake16@gmail.com>
2026-04-23 12:12:15 +0100

    Better big O solution

--- Neetcode/Blind_75/valid_anagram.py
+++ Neetcode/Blind_75/valid_anagram.py
@@ -4,6 +4,7 @@ Difficulty: Easy
 Given two strings s and t, return true if t is an anagram of s, and false otherwise. e.g. "anagram" and "nagaram" are anagrams, while "rat" and "car" are not.
 """
 
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
