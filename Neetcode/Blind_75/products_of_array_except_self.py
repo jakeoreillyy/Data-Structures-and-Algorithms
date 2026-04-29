1559f8ed3b48ea71294d27e0b1f0b316985658c4
Jake <oreillyjake16@gmail.com>
2026-04-29 12:25:44 +0100

    Solved product of array except self

---
+++ Neetcode/Blind_75/products_of_array_except_self.py
@@ -0,0 +1,35 @@
+"""
+Product of Array Except Self
+Difficulty: Medium
+
+Description:
+    Given an integer array nums, return an array answer such that answer[i]
+    is equal to the product of all the elements of nums except nums[i].
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+Args:
+    nums (List[int]): Array of integers.
+
+Returns:
+    List[int]: Array where each element is the product of all elements in nums except the corresponding element.
+
+Time Complexity:  O(n)
+Space Complexity: O(n)
+"""
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
